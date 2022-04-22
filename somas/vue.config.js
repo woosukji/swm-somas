59fabf4cbf9751804d0f82049484c2b9fb732706
woosukji <woosukji@snu.ac.kr>
2022-04-22 14:29:13 +0900

    setting CI/CD

--- somas/vue.config.js
+++ somas/vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify',
-  ],
+  publicPath: process.env.NODE_ENV === 'production' ? '/swm-somas/' : '/',
+
+  transpileDependencies: ['vuetify'],
 });
