457116dc6a5dd1964af4c5d2829b7bba9f406706
Maeg5854 <kimhj585407@gmail.com>
2022-05-01 20:12:13 +0900

    - rewrite firebase.js - add actions into src/store/index.js

--- somas/src/plugins/firebase.js
+++ somas/src/plugins/firebase.js
@@ -1,21 +1,22 @@
-// API키와 같은 민감 정보를 secret_config.json 파일로 분리했습니다.
-// secret_config.json 파일을 로드해서 코드에는 간접적으로 입력합니다.
+// API키와 같은 민감 정보를 secretConfig.json 파일로 분리했습니다.
+// secretConfig.json 파일을 로드해서 코드에는 간접적으로 입력합니다.
 import { initializeApp } from 'firebase/app';
-import { getAuth } from "firebase/auth";
-import { getDocs, getFirestore } from "firebase/firestore"
-const secret_config = require("../../../config/secret_config.json");
+import { getDatabase } from 'firebase/database';
+import { getStorage } from 'firebase/storage';
+
+const secretConfig = require('../../config/secret_config.json');
 
 const firebaseConfig = {
-    apiKey: secret_config.apiKey,
-    authDomain: secret_config.authDomain,
-    databaseURL: secret_config.databaseURL,
-    projectId: secret_config.projectId,
-    storageBucket: secret_config.storageBucket,
-    messagingSenderId: secret_config.messagingSenderId,
-    appId: secret_config.appId,
-    measurementId: secret_config.measurementId
+  apiKey: secretConfig.apiKey,
+  authDomain: secretConfig.authDomain,
+  databaseURL: secretConfig.databaseURL,
+  projectId: secretConfig.projectId,
+  storageBucket: secretConfig.storageBucket,
+  messagingSenderId: secretConfig.messagingSenderId,
+  appId: secretConfig.appId,
+  measurementId: secretConfig.measurementId,
 };
-  
-const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+export const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app); // 파이어베이스 실시간 데이터베이스
+export const storage = getStorage(app); // 파이어베이스 스토리지
--- somas/src/store/index.js
+++ somas/src/store/index.js
@@ -1,7 +1,9 @@
-import { getDocs } from 'firebase/firestore';
+import { onValue, ref as dbRef, set } from 'firebase/database';
+import { uploadBytes, ref as storageRef, deleteObject } from 'firebase/storage';
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { db } from '../plugins/firebase';
+import { db, storage } from '../plugins/firebase';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
@@ -12,17 +14,89 @@ export default new Vuex.Store({
   mutations: {
   },
   actions: {
-
-    /**
-     * 전체 유저 리스트를 반환합니다.
-     * @returns 유저 리스트
-     */
+    async signIn(id) {
+      const userRef = db.ref(`user/${id}`);
+      const userSnapshot = await userRef.once('value');
+      const user = userSnapshot.val();
+      if (!user) {
+        console.log('Not registered user');
+        return null;
+      }
+      console.log('Successfully login');
+      return 1;
+    },
+    async signUp(userId, name, imageUrl) {
+      set(dbRef(db, `user/${userId}`), {
+        name: `${name}`,
+        profile_image: imageUrl,
+        tag: { imageUrl },
+      });
+    },
     async getUsers() {
-      const usersCol = collection(db, 'users');
-      const userSnapshot = await getDocs(usersCol);
-      const userList = userSnapshot.docs.map(doc => doc.data());
-      return userList;
-    }
+      const userRef = dbRef(db, 'user/');
+      onValue(userRef, (snapshot) => {
+        const data = snapshot.val();
+        if (!data) {
+          console.log('no data in user');
+          return null;
+        }
+        return data;
+      });
+    },
+    async getMeetings() {
+      const meetingsRef = db.ref('meeting');
+      const meetingsSnapshot = await meetingsRef.once('value');
+      const meetings = meetingsSnapshot.val();
+      if (!meetings) {
+        console.log('no data in meetings');
+        return null;
+      }
+      return meetings;
+    },
+    async getMeetingWithId(meetingId) {
+      const meetingRef = db.ref(`meeting/${meetingId}`);
+      const meetingSnapshot = await meetingRef.once('value');
+      return meetingSnapshot.val();
+    },
+    async getPosts() {
+      const postsRef = db.ref('post/');
+      const postsSnapshot = await postsRef.once('value');
+      return postsSnapshot.val();
+    },
+    async getPostWithId(postId) {
+      const postRef = db.ref(`post/${postId}`);
+      const postSnapshot = await postRef.once('value');
+      return postSnapshot.val();
+    },
+    async getPostIdsWithTag(tag) {
+      const postIdRef = db.ref(`tag/${tag}/post`);
+      const postIdSnapshot = await postIdRef.once('value');
+      return postIdSnapshot.val();
+    },
+    async uploadProfileImage(imgName, file) {
+      const imageRef = storageRef(storage, `public/profile_image/'${imgName}`);
+      uploadBytes(imageRef, file).then((snapshot) => {
+        console.log('Uploaded a blob or file!', snapshot);
+      });
+      return imageRef.bucket + imageRef.fullPath;
+    },
+    async uploadPostImage(imgName, file) {
+      const imagePath = `public/post_image/${imgName}`;
+      const imageRef = storageRef(storage, imagePath);
+      uploadBytes(imageRef, file).then((snapshot) => {
+        console.log('Uploaded a blob or file!', snapshot);
+      });
+
+      return imageRef.bucket + imageRef.fullPath;
+    },
+    async deleteFile(imgPath) {
+      const imageRef = storageRef(storage, imgPath);
+      deleteObject(imageRef).then(() => {
+        console.log('file deleted successfully');
+      }).catch((e) => {
+        console.error(e);
+      });
+    },
   },
   modules: {
   },
